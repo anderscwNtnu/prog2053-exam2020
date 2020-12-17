2c0603438924f072fea36836b557bf4842545459
Anders Christoffer Westby <anderscw@stud.ntnu.no>
2020-12-17 12:38:35 +0100

    oppgave3 display values

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -4,10 +4,30 @@ class EditUser extends LitElement {
   static get properties() {
     return {
       user: { type: Object }
+
     };
   }
 
   // din kode her
 
+
+  //  fetch("api/fetchUsers.php") Already done
+  // 
+
+  render() {
+   return html`
+
+     <!-- Display all fields to change -->
+    <form class= "updateUserInfo">
+      <p> Username: ${this.uname} </p>
+      <p> First Name: ${this.firstName} </p>
+      <p> Last Name: ${this.lastName} </p>
+      <p> old password: ${this.oldPwd} </p>
+      <p> new password: ${this.pwd} </p>
+
+      <!-- Submit button  can be implemented-->
+    </form>
+    `
+  }
 }
 customElements.define('edit-user', EditUser);
